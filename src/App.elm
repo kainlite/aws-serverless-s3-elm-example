f75f8a994f1e2b4b67b35f9f538595dd179be754
kainlite <kainlite@gmail.com>
2018-02-17 16:08:35 -0300

    Basic form working

--- src/App.elm
+++ src/App.elm
@@ -3,9 +3,134 @@ module App exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+import Json.Decode as Json
+import Json.Encode
 
 
+type alias Model =
+    { newMessage : Message
+    }
+
+
+emptyModel : Model
+emptyModel =
+    { newMessage = emptyNewMessage
+    }
+
+
+emptyNewMessage =
+    Message "" ""
+
+
+type alias Message =
+    { email : String
+    , message : String
+    }
+
+
+type Msg
+    = SendMessage
+    | UpdateMessage Message
+    | SendMessageHttp (Result Http.Error Message)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UpdateMessage newMessage ->
+            ( { model | newMessage = newMessage }, Cmd.none )
+
+        SendMessage ->
+            let
+                newMessage =
+                    Debug.log "model.newMessage" model.newMessage
+            in
+            ( { model | newMessage = emptyNewMessage }, postMessage newMessage )
+
+        SendMessageHttp (Ok response) ->
+            let
+                _ =
+                    Debug.log "response" response
+            in
+            ( model, Cmd.none )
+
+        SendMessageHttp (Err err) ->
+            let
+                _ =
+                    Debug.log "err" err
+            in
+            ( model, Cmd.none )
+
+
+decodeMessage : Json.Decoder Message
+decodeMessage =
+    Json.map2 Message
+        (Json.field "id" Json.string)
+        (Json.field "message" Json.string)
+
+
+postMessage newMessage =
+    Http.send SendMessageHttp
+        (Http.post "https://csunn71hu7.execute-api.us-east-1.amazonaws.com/dev/sendMail"
+            (encodeNewMessage newMessage)
+            decodeMessage
+        )
+
+
+encodeNewMessage : Message -> Http.Body
+encodeNewMessage newMessage =
+    Http.jsonBody <|
+        Json.Encode.object
+            [ ( "email", Json.Encode.string newMessage.email )
+            , ( "message", Json.Encode.string newMessage.message )
+            ]
+
+
+isEmpty : String -> Bool
+isEmpty =
+    String.isEmpty << String.trim
+
+
+main : Program Never Model Msg
 main =
+    Html.program
+        { view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , init = ( emptyModel, Cmd.none )
+        }
+
+
+viewForm newMessage toUpdateMessage sendMessage =
+    Html.form [ class "form", onSubmit SendMessage, action "javascript:void(0);" ]
+        [ h2 [ id "contact", class "title" ] [ text "Contact us" ]
+        , label [ class "label" ] [ text "Your email" ]
+        , input
+            [ class "input"
+            , placeholder "Your email"
+            , value newMessage.email
+            , onInput (\v -> toUpdateMessage { newMessage | email = v })
+            ]
+            []
+        , label [ class "label" ] [ text "Message" ]
+        , textarea
+            [ class "textarea"
+            , placeholder "Let us know how we can help you..."
+            , value newMessage.message
+            , onInput (\v -> toUpdateMessage { newMessage | message = v })
+            ]
+            []
+        , button
+            [ class "button is-primary has-text-centered"
+            , type_ "submit"
+            , disabled <| isEmpty newMessage.email || isEmpty newMessage.message
+            ]
+            [ text "Send" ]
+        ]
+
+
+view model =
     div []
         [ section [ class "hero is-info is-medium is-bold" ]
             [ div [ class "hero-head" ]
@@ -122,16 +247,7 @@ main =
             [ class "container" ]
             [ br [] []
             , div [ class "columns" ]
-                [ div [ class "column auto" ]
-                    [ Html.form [ class "form", method "post", action "https://p6c2wzb7s3.execute-api.us-east-1.amazonaws.com/dev/sendMail" ]
-                        [ h2 [ id "contact", class "title" ] [ text "Contact us" ]
-                        , label [ class "label" ] [ text "Your email" ]
-                        , input [ class "input", placeholder "Your email" ] []
-                        , label [ class "label" ] [ text "Message" ]
-                        , textarea [ class "textarea", placeholder "Let us know how we can help you..." ] []
-                        , button [ class "button is-primary has-text-centered" ] [ text "Send" ]
-                        ]
-                    ]
+                [ div [ class "column auto" ] <| [ viewForm model.newMessage UpdateMessage SendMessage ]
                 ]
             ]
         , br [] []
